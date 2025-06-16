a0e880334499d44730c95fc2b9d7806fc585d9cb
Lucian Smith <luxianosu@gmail.com>
2025-06-16 11:13:57 -0600

    Swagger API documentation (#4)

--- jdk1.8/Springboot/users-apiXmongo-db/users-api/README.md
+++ jdk1.8/Springboot/users-apiXmongo-db/users-api/README.md
@@ -1,13 +1,25 @@
-# This is simple springboot app using jdk 1.8
+# This is simple Spring Boot API using jdk 1.8
 
-Checklist for this practice
+## Checklist for this practice
 
 - [x] Run API locally
 - [x] Connect to MongoDB local instance
 - [x] Dockerize API
 - [x] Unit testing
   - Junit5
+- [x] Swagger File
 - [ ] Error handling
 - [ ] Logging
 - [ ] Security (security access, basic auth, jwt, oauth2, etc)
 - [ ] Practice dynamic replica scales with helm and kubernetes
+
+## Run with docker
+- `mvn clean package`
+- `docker build -t users-api -f docker/Dockerfile .`
+- `docker run -p 8080:8080 users-api`
+
+## Run with only Spring Boot embedded tomcat server
+- `mvn spring-boot:run `
+
+## API Documentation
+- [Swagger](http://localhost:8080/swagger-ui/index.html)
--- jdk1.8/Springboot/users-apiXmongo-db/users-api/src/main/java/com/practice/java/users/api/controller/UserController.java
+++ jdk1.8/Springboot/users-apiXmongo-db/users-api/src/main/java/com/practice/java/users/api/controller/UserController.java
@@ -2,6 +2,8 @@ package com.practice.java.users.api.controller;
 
 import com.practice.java.users.api.model.User;
 import com.practice.java.users.api.respository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,28 +13,33 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
+@Tag(name = "User API", description = "User CRUD maintenance API")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
     @GetMapping
+    @Operation(summary = "Get all Users")
     public List<User> getAll(){
         return userRepository.findAll();
     }
 
     @GetMapping("/{email}")
+    @Operation(summary = "Get user by email")
     public User getUserByEmail(@PathVariable String email) {
         Optional<User> user = userRepository.findByEmail(email);
         return user.orElse(null);
     }
 
     @PostMapping
+    @Operation(summary = "Create new user")
     public User createUser(@RequestBody User user){
         return userRepository.save(user);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update an existent user")
     public ResponseEntity<User> updateUser(@PathVariable String id, @RequestBody User userToUpdate) {
         return userRepository.findById(id)
                 .map(user -> {
@@ -47,6 +54,7 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deletes an user by id")
     public void deleteUser(@PathVariable String id) {
         userRepository.deleteById(id);
     }
