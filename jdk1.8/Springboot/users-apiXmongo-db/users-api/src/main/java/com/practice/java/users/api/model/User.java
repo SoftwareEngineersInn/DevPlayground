ef561d4d0cf534c8b9cc91ff7823d261e9799b97
Lucian Smith <luxianosu@gmail.com>
2025-06-16 11:12:58 -0600

    Swagger API documentation

--- jdk1.8/Springboot/users-apiXmongo-db/users-api/src/main/java/com/practice/java/users/api/model/User.java
+++ jdk1.8/Springboot/users-apiXmongo-db/users-api/src/main/java/com/practice/java/users/api/model/User.java
@@ -1,5 +1,6 @@
 package com.practice.java.users.api.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -7,8 +8,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Data
 @Document(collection = "users")
 public class User {
+
     @Id
+    @Schema(hidden = true)
     private String id;
+
     private String name;
     private String email;
     private int age;
