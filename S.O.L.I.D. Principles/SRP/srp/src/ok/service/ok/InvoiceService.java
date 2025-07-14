fea7f334dbf5764c98749a4bffe3fc2eea8031dd
Lucian Smith <luxianosu@gmail.com>
2025-07-14 14:53:37 -0600

    Update InvoiceService.java

--- S.O.L.I.D. Principles/SRP/srp/src/ok/service/ok/InvoiceService.java
+++ S.O.L.I.D. Principles/SRP/srp/src/ok/service/ok/InvoiceService.java
@@ -29,17 +29,4 @@ public class InvoiceService {
         printer.printBill();
     }
 }
-/**
- * This works, and it's valid for small and simple projects that not pretend to get
- * more complexity in the near future.
- * <p>
- * There exists to potential problems if this application grows:
- * <p>
- * - High Coupling
- * If InvoicePrinter changes its constructor method or a new dependency is required. We will need to update our
- * Invoice service class as well
- * <p>
- * - Testing difficulty
- * This approach will add extra difficulty to create mock for testing because it creates its own dependencies.
- **/
 
