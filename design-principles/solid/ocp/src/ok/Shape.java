7fd9679f1e303bf755cc5a98d2c9ccbe4a5059f5
Lucian Smith <luxianosu@gmail.com>
2025-07-29 13:55:57 -0600

    OCP FIX

---
+++ design-principles/solid/ocp/src/ok/Shape.java
@@ -0,0 +1,14 @@
+package ok;
+
+/**
+ * the getArea method will be the common function between shapes
+ * so let's create an interface that can be implemented by any Shape Class
+ * **/
+public interface Shape {
+    double getArea();
+}
+/**
+ * This method will guarantee the use of OCP
+ * since each shape will be implemented it to calculate the area,
+ * and it will not affect already-proved code.
+ */
