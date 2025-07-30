c39ef50f34dc07fe85fb8110ca4b813eb0df2a32
Lucian Smith <luxianosu@gmail.com>
2025-07-29 19:09:09 -0600

    Update Main.java

--- design-principles/solid/ocp/src/main/java/org/software/engineers/inn/solid/Main.java
+++ design-principles/solid/ocp/src/main/java/org/software/engineers/inn/solid/Main.java
@@ -3,5 +3,6 @@ package org.software.engineers.inn.solid;
 public class Main {
     public static void main(String[] args) {
         //TODO
+        System.out.println("Hello and welcome");
     }
 }
