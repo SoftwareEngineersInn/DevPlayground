46c5d1a198765eeb3c5383c552bdbce6ca1a5d07
Lucian Smith <luxianosu@gmail.com>
2025-07-29 22:11:23 -0600

    OCP fix .gitignore

---
+++ design-principles/solid/ocp/src/main/java/org/software/engineers/inn/solid/Main.java
@@ -0,0 +1,8 @@
+package org.software.engineers.inn.solid;
+
+public class Main {
+    public static void main(String[] args) {
+        //TODO
+        System.out.println("Hello and welcome");
+    }
+}
---
+++ design-principles/solid/ocp/src/main/java/org/software/engineers/inn/solid/ok/Circle.java
@@ -0,0 +1,23 @@
+package org.software.engineers.inn.solid.ok;
+
+/**
+ * Same approach from the Square class
+ * Notice how the get area changes its formula and by this approach
+ * Only the shape class will set the way it calculates its own area
+ * **/
+public class Circle implements Shape{
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
+/**
+ * As longer as you create new shapes, you will set the responsibility of calculate
+ * each area to each new form instead of modifying a huge 'if' structure as the
+ * wrong example shows us.
+ * **/
