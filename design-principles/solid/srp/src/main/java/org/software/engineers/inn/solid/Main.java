a23522313218b7894743b446b9bc90ea1ca266d7
Lucian Smith <luxianosu@gmail.com>
2025-07-29 22:09:23 -0600

    SRP fix .gitnore

---
+++ design-principles/solid/srp/src/main/java/org/software/engineers/inn/solid/Main.java
@@ -0,0 +1,7 @@
+package org.software.engineers.inn.solid;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+    }
+}
---
+++ design-principles/solid/srp/src/main/java/org/software/engineers/inn/solid/wrong/InvoiceManager.java
@@ -0,0 +1,8 @@
+package org.software.engineers.inn.solid.wrong;
+
+//Single class with several responsibilities
+public class InvoiceManager {
+    public void calculateBillTotal(){}
+    public void printBill(){}
+    public void saveToDb(){}
+}
