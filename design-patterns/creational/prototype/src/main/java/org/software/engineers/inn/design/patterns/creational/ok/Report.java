46d2d7383bcc9d9a7066eee62556728b51ea1d02
Lucian Smith <luxianosu@gmail.com>
2025-08-27 02:24:47 -0600

    prototype progress II

--- design-patterns/creational/prototype/src/main/java/org/software/engineers/inn/design/patterns/creational/ok/Report.java
+++ design-patterns/creational/prototype/src/main/java/org/software/engineers/inn/design/patterns/creational/ok/Report.java
@@ -3,6 +3,8 @@ package org.software.engineers.inn.design.patterns.creational.ok;
 /**
  * Prototype Solution
  *
+ * Step 1: Implements Cloneable
+ *
  * You create a base prototype, and we clone it everytime we need a new Report
  * **/
 public class Report implements Cloneable {
@@ -28,4 +30,21 @@ public class Report implements Cloneable {
     }
 
     // setters and getters
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
